a874772ce45ef5558e4b95f5422251ad43b249e9
davidv408 <davidv408@github.com>
2025-07-12 17:42:17 -0700

    Add PressStart 2P Font and revise text

--- src/components/StartGameModal/StartGameModal.tsx
+++ src/components/StartGameModal/StartGameModal.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react";
 import Modal from "../Modal/Modal";
+import styles from "./StartGameModal.module.css";
 
 interface StartGameModalProps {
   onStart: () => void;
@@ -12,19 +13,35 @@ const StartGameModal: FC<StartGameModalProps> = ({ onStart }) => {
       body={
         <div>
           <p>
-            In Tetris, shapes called tetrominoes fall from the top of the
-            screen. Your goal is to rotate and move them left or right to fit
-            them together and form complete horizontal lines. When a line is
-            filled, it disappears, and you earn points. The game ends when the
-            stack of pieces reaches the top.
+            Fit the falling pieces to clear lines and prevent the stack from
+            reaching the top!
           </p>
           <h3>Controls</h3>
-          <ul>
+          <ul className={styles.controlList}>
             <li>
-              Left/Right/Down Arrow - Move the piece left, right, or down one
-              space.
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+              </svg>
+              Move Left
             </li>
-            <li>Up Arrow - Rotate the pice.</li>
+            <li>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+              </svg>
+              Move Rignt
+            </li>
+            <li>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+              </svg>
+              Move Down
+            </li>
+            <li>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2 160 448c0 17.7 14.3 32 32 32s32-14.3 32-32l0-306.7L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
+              </svg>
+              Rotate
+            </li>
           </ul>
         </div>
       }
